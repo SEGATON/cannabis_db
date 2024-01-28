ae575a32c06d0a2f1f28a9a6ba4c300988b6a866
SEGATON <90114691+SEGATON@users.noreply.github.com>
2024-01-28 17:21:57 -0500

    nc

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,177 @@
 
 let map;
 
-async function initiateTheMap() {
+async function initMap() {
 const { Map } = await google.maps.importLibrary("maps");
 
-map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById("map"), {
     center: { lat: 40.74527784245027, lng: -73.99032458586362 },
     mapId:'',
     zoom: 14,
     styles: [
-                {
-                    "featureType": "all",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#000"
-                        }
-                    ]
-                },
-                {},
-        ]
+          {
+              "featureType": "all",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                  {
+                      "color": "#ffffff"
+                  }
+              ]
+          },
+          {
+              "featureType": "all",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  },
+                  {
+                      "color": "#333739"
+                  },
+                  {
+                      "weight": 0.8
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  }
+              ]
+          },
+          {
+              "featureType": "poi",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  },
+                  {
+                      "lightness": -7
+                  }
+              ]
+          },
+          {
+              "featureType": "poi.attraction",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "poi.business",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "poi.park",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#070a09"
+                  },
+                  {
+                      "lightness": "-1"
+                  },
+                  {
+                      "gamma": "1.89"
+                  }
+              ]
+          },
+          {
+              "featureType": "road",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#333739"
+                  },
+                  {
+                      "weight": 0.3
+                  },
+                  {
+                      "lightness": 10
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  },
+                  {
+                      "lightness": -28
+                  }
+              ]
+          },
+          {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  },
+                  {
+                      "visibility": "on"
+                  },
+                  {
+                      "lightness": -15
+                  }
+              ]
+          },
+          {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  },
+                  {
+                      "lightness": -18
+                  }
+              ]
+          },
+          {
+              "featureType": "transit",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#2ecc71"
+                  },
+                  {
+                      "lightness": -34
+                  }
+              ]
+          },
+          {
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#333739"
+                  }
+              ]
+          }
+      ]
   });
 }
+
